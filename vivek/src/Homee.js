d1ae3e5b7e52e45188354fbd9584e756b2699d52
unknown <vivekvardhandupally@gmail.com>
2023-07-15 15:36:16 +0530

    first

---
+++ vivek/src/Homee.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+export default class Homee extends Component{
+    render(){
+        return(
+        <div>
+                <a href="https://www.google.com/">female Shopping</a>
+        </div>
+        );
+}
+}
---
+++ vivek/src/Men.js
@@ -0,0 +1,10 @@
+import React,{Component} from "react";
+export default class Men    extends Component{
+    render(){
+        return(
+            <div>
+            <a href="https://www.google.com/">Men Shopping</a>
+            </div>
+        )
+    }
+}
